a086af2c7ff44c3781f262c05fcf7160d9ffdacc
Yawei Li <yawei1992@gmail.com>
2016-05-26 20:55:57 -0400

    Set up empty project. Added Utility class for Yelp Api keys.

---
+++ app/src/main/java/com/liyawei/yelpdemo/Utility.java
@@ -0,0 +1,12 @@
+package com.liyawei.yelpdemo;
+
+/**
+ * Created by liyawei
+ */
+public class Utility {
+    //Yelp Api Keys
+    public static final String CONSUMER_KEY = "f4vMEeS7KzfUkTFKy9kEsA";
+    public static final String CONSUMER_SECRET = "hvi1sGzIp7dqsRBAk4deGv4o5SM";
+    public static final String TOKEN = "VUvr5izi5ccNJLi3qakfhIe3KbYM1MrJ";
+    public static final String TOKEN_SECRET = "0IwGouR9DGgRfMDQLznI0VFNK2w";
+}
