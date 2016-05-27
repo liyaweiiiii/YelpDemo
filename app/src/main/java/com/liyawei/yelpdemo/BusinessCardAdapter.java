e0078ea5ea3d3e78607baae024bdaa6332eee181
Yawei Li <yawei1992@gmail.com>
2016-05-27 09:26:36 -0400

    Add list of restaurant

---
+++ app/src/main/java/com/liyawei/yelpdemo/BusinessCardAdapter.java
@@ -0,0 +1,71 @@
+package com.liyawei.yelpdemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yelp.clientlib.entities.Business;
+
+import java.util.List;
+
+/**
+ * Created by liyawei.
+ */
+public class BusinessCardAdapter extends RecyclerView.Adapter<BusinessCardAdapter.BusinessViewHolder> {
+    public static final String LOG_TAG = BusinessCardAdapter.class.getSimpleName();
+    private List<Business> businessList;
+
+    public BusinessCardAdapter(List<Business> businessList) {
+        this.businessList = businessList;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return businessList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(BusinessViewHolder businessViewHolder, int i) {
+        Business business = businessList.get(i);
+        businessViewHolder.vName.setText(business.name());
+        businessViewHolder.vPhone.setText(business.phone());
+        businessViewHolder.vAddress.setText(business.location().address().get(0));
+        businessViewHolder.vImage.setImageResource(R.mipmap.ic_launcher);
+    }
+
+    @Override
+    public BusinessViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.business_card, viewGroup, false);
+
+        return new BusinessViewHolder(itemView);
+    }
+
+    public static class BusinessViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        protected ImageView vImage;
+        protected TextView vName;
+        protected TextView vPhone;
+        protected TextView vAddress;
+
+        public BusinessViewHolder(View v) {
+            super(v);
+            vName =  (TextView) v.findViewById(R.id.business_name);
+            vPhone = (TextView)  v.findViewById(R.id.business_phone);
+            vAddress = (TextView)  v.findViewById(R.id.business_addr);
+            vImage = (ImageView) v.findViewById(R.id.business_thumb);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.d(LOG_TAG, "onClick " + getAdapterPosition());
+        }
+    }
+}
--- app/src/main/java/com/liyawei/yelpdemo/MainActivity.java
+++ app/src/main/java/com/liyawei/yelpdemo/MainActivity.java
@@ -2,12 +2,99 @@ package com.liyawei.yelpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends AppCompatActivity {
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.Business;
+import com.yelp.clientlib.entities.SearchResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+    YelpAPIFactory apiFactory = new YelpAPIFactory(Utility.CONSUMER_KEY, Utility.CONSUMER_SECRET, Utility.TOKEN, Utility.TOKEN_SECRET);
+    YelpAPI yelpAPI = apiFactory.createAPI();
+
+    SearchView searchView;
+    RecyclerView recList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            recList = (RecyclerView) findViewById(R.id.cardList);
+            recList.setHasFixedSize(true);
+
+            LinearLayoutManager llm = new LinearLayoutManager(this);
+            llm.setOrientation(LinearLayoutManager.VERTICAL);
+            recList.setLayoutManager(llm);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        searchView.clearFocus();
+        Map<String, String> params = new HashMap<>();
+
+
+        params.put("term", query);
+        params.put("sort", "2");
+        params.put("limit", "10");
+        params.put("lang", "en");
+
+        Call<SearchResponse> call = yelpAPI.search("Toronto", params);
+        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+            @Override
+            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+                SearchResponse searchResponse = response.body();
+                // Update UI text with the searchResponse.
+
+                handleResponce(searchResponse);
+            }
+
+            @Override
+            public void onFailure(Call<SearchResponse> call, Throwable t) {
+                // HTTP error happened, do something to handle it.
+            }
+        };
+
+        call.enqueue(callback);
+        return true;
+    }
+
+    private void handleResponce(SearchResponse searchResponse) {
+        ArrayList<Business> businesses = searchResponse.businesses();
+        int totalNumberOfResult = searchResponse.total();
+
+        BusinessCardAdapter bca = new BusinessCardAdapter(businesses);
+        recList.setAdapter(bca);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        return false;
     }
 }
